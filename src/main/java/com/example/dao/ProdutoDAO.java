582e4b483a8906e3eec59ba8fff11e1221cedae2
Edson Parisotto <parisotto@gmail.com>
2023-08-20 19:36:59 -0300

    Create ProdutoDAO.java

---
+++ src/main/java/com/example/dao/ProdutoDAO.java
@@ -0,0 +1,36 @@
+package com.example.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.example.model.Produto;
+
+public class ProdutoDAO extends DAO {
+    public ProdutoDAO(Connection conn) {
+        super(conn);
+    }
+    
+    public void excluir(long id) {
+        var sql = "delete from produto where id = ?";
+        try {
+            var statement = conn.prepareStatement(sql);
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro ao excluir o produto: " + e.getMessage());
+        }
+        
+    }
+
+    public void inserir(Produto produto) {
+        var sql = "insert into produto (nome, marca_id, valor) values (?, ?, ?)";
+        try(var statement = conn.prepareStatement(sql)) {
+            statement.setString(1, produto.getNome());
+            statement.setLong(2, produto.getMarca().getId());
+            statement.setDouble(3, produto.getValor());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro na execução da consulta: " + e.getMessage());
+        }
+    }
+}
